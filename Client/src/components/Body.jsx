35e3dbd6ddc8ae049c16c49f4a2240b8da515106
Om Jaiswal <107876549+omjaiswal45@users.noreply.github.com>
2025-07-25 01:18:31 +0530

    Update Body.jsx

--- Client/src/components/Body.jsx
+++ Client/src/components/Body.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
-import Sidebar from './Sidebar';
+import Sidebar from './SIdebar';
 import Header from './Header';
 import Dashboard from './Dashboard';
 import Logs from '../pages/Logs';
